d7071a93a086ce28eafbfde36b7dae3324f7ce8c
benbellot26 <benjamin.bellot.bb@gmail.com>
2026-08-14 10:26:42 +0200

    chore: remove obsolete backtest wrapper

--- v11_backtest.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-from v11.backtest import write_report
-
-if __name__ == "__main__":
-    import json
-    report = write_report()
-    print(json.dumps(report, ensure_ascii=False, indent=2, sort_keys=True))
