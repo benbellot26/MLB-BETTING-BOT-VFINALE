df6be2d89f2eb7e9bfe3d192bf83f84b18ed8fe0
benbellot26 <benjamin.bellot.bb@gmail.com>
2026-08-25 09:47:10 +0200

    V14: include native MLB input tests in preflight

--- v14/preflight.py
+++ v14/preflight.py
@@ -9,6 +9,7 @@ CRITICAL_TEST_MODULES = (
     "tests.test_v14_discord",
     "tests.test_v14_feature_row",
     "tests.test_v14_market_edge",
+    "tests.test_v14_mlb_inputs",
     "tests.test_v14_pipeline",
     "tests.test_v14_production_runtime",
     "tests.test_v14_run_stack_parity",
