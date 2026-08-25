19c93f4f3da2296ca7a9e21287d8b097b21e043c
benbellot26 <benjamin.bellot.bb@gmail.com>
2026-08-25 17:20:52 +0200

    V14: bump hardened model generation

--- v14/__init__.py
+++ v14/__init__.py
@@ -1,7 +1,7 @@
 """Pulsar V14 production model package."""
 
-VERSION = "14.1.0"
-MODEL_GENERATION = "pulsar-v14-context-v2"
-SCHEMA = "pulsar-v14-probability-v1"
+VERSION = "14.2.0"
+MODEL_GENERATION = "pulsar-v14-context-v3"
+SCHEMA = "pulsar-v14-probability-v2"
 
 __all__ = ["VERSION", "MODEL_GENERATION", "SCHEMA"]
