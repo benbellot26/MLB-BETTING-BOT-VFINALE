3c91f1b015cfda2d31e9cacf45e74ff080761601
benbellot26 <benjamin.bellot.bb@gmail.com>
2026-08-25 23:43:54 +0200

    V14: include starter fallback in preflight

--- v14/preflight.py
+++ v14/preflight.py
@@ -24,6 +24,7 @@ CRITICAL_TEST_MODULES=(
     "tests.test_v14_pipeline",
     "tests.test_v14_production_runtime",
     "tests.test_v14_run_stack_parity",
+    "tests.test_v14_starter_fallback",
     "tests.test_v14_starter_integrity",
     "tests.test_v14_structural_parity",
     "tests.test_v14_tracking",
