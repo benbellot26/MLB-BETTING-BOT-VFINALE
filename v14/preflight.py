24016e34dab5a85b609327d8dc7467e151b20f72
benbellot26 <benjamin.bellot.bb@gmail.com>
2026-08-26 12:04:23 +0200

    V14: include professional hardening in preflight

--- v14/preflight.py
+++ v14/preflight.py
@@ -23,6 +23,7 @@ CRITICAL_TEST_MODULES=(
     "tests.test_v14_phase",
     "tests.test_v14_pipeline",
     "tests.test_v14_production_runtime",
+    "tests.test_v14_professional_hardening",
     "tests.test_v14_run_stack_parity",
     "tests.test_v14_starter_fallback",
     "tests.test_v14_starter_integrity",
